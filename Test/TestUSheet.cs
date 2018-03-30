fa6a2b6802bdbc6b2f7a9907a4f838818d86a9e6
zhangwen <i.am.zw129@gmail.com>
2018-03-30 17:07:49 +0800

    不忽略Test中的meta文件

--- Test/TestUSheet.cs
+++ Test/TestUSheet.cs
@@ -4,126 +4,141 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-using USheet;
-
-public class TestUSheet : MonoBehaviour {
-
-    public string dataPath = "Assets/Resource/Student.asset";
-
-    public InputField inputField;
-    public Button getRow1Btn;
-    public Button getRow2Btn;
-
-    public Button modifyBtn;
-
-    public Text output;
-
-    private SheetData _data = null;
-
-    // Use this for initialization
-    void Start () {
-
-        _data = AssetDatabase.LoadAssetAtPath<SheetData>(dataPath);
-        if (_data == null)
-        {
-            Debug.LogError("load sheet data error");
-            return;
-        }
-
-        if (getRow1Btn != null)
-        {
-            getRow1Btn.onClick.AddListener(onTestGetRowBtnClick);
-        }
-        if (getRow2Btn != null)
-        {
-            getRow2Btn.onClick.AddListener(onGetRow2BtnClick);
-        }
-        if (modifyBtn != null)
-        {
-            modifyBtn.onClick.AddListener(onModifyBtnClick);
-        }
-
-        output.text = "row:" + _data.rowCount + ", col:" + _data.columnCount;
-	}
-
-    private string formatRow(Dictionary<string, IGridData> rowData)
+namespace USheet
+{
+    public class TestUSheet : MonoBehaviour
     {
-        if (rowData == null) return "null";
+        public SheetData data = null;
+        public string dataPath = "Assets/Resource/Student.asset";
 
-        string outputText = "";
-        foreach (var item in rowData)
-        {
-            outputText += string.Format("{0}:{1}\n", item.Key, item.Value.ToString());
-        }
-        return outputText;
-    }
 
-    private void onTestGetRowBtnClick()
-    {
-        if (_data == null)
-        {
-            Debug.LogError("Sheet is null");
-            return;
-        }
-        string rowStr = inputField.text;
-        if (String.IsNullOrEmpty(rowStr))
-        {
-            output.text = "Input: row number";
-            return;
-        }
-        Dictionary<string, IGridData> rowData = _data.getRow(int.Parse(rowStr));
-        output.text = formatRow(rowData);
-    }
 
-    private void onGetRow2BtnClick()
-    {
-        if (_data == null)
+        public InputField inputField;
+        public Button getRow1Btn;
+        public Button getRow2Btn;
+
+        public Button modifyBtn;
+
+        public Text output;
+
+
+        // Use this for initialization
+        void Start()
         {
-            Debug.LogError("Sheet is null");
-            return;
-        }
-        string rowStr = inputField.text;
-        if (String.IsNullOrEmpty(rowStr))
-        {
-            output.text = "Input: title|value (only string)";
-            return;
-        }
-        string[] searchParams = rowStr.Split('|');
-        if (searchParams.Length != 2)
-        {
-            Debug.LogError("searchParams error");
-            return;
-        }
-        List<Dictionary<string, IGridData>> rows = _data.getRows(searchParams[0], searchParams[1]);
-        if (rows == null)
-        {
-            output.text = "No result";
-            return;
+            if (data == null)
+            {
+                data = AssetDatabase.LoadAssetAtPath<SheetData>(dataPath);
+                if (data == null)
+                {
+                    Debug.LogWarning("load sheet data error");
+                    return;
+                }
+            }
+            
+            if (data == null)
+            {
+                Debug.LogWarning("sheet data is null");
+                return;
+
+            }
+
+            if (getRow1Btn != null)
+            {
+                getRow1Btn.onClick.AddListener(onTestGetRowBtnClick);
+            }
+            if (getRow2Btn != null)
+            {
+                getRow2Btn.onClick.AddListener(onGetRow2BtnClick);
+            }
+            if (modifyBtn != null)
+            {
+                modifyBtn.onClick.AddListener(onModifyBtnClick);
+            }
+
+            output.text = "row:" + data.rowCount + ", col:" + data.columnCount;
         }
 
-        string outputStr = "";
-        for (int i = 0; i < rows.Count; i++)
+        private string formatRow(Dictionary<string, IGridData> rowData)
         {
-            outputStr += formatRow(rows[i]);
-            outputStr += "-----------------------------------\n";
+            if (rowData == null) return "null";
+
+            string outputText = "";
+            foreach (var item in rowData)
+            {
+                outputText += string.Format("{0}:{1}\n", item.Key, item.Value.ToString());
+            }
+            return outputText;
         }
 
-        output.text = outputStr;
-    }
-
-    private void onModifyBtnClick()
-    {
-        string[] searchParams = inputField.text.Split('|');
-        if (searchParams.Length != 3)
+        private void onTestGetRowBtnClick()
         {
-            output.text = "Input: rowNo|colName|value (only string)";
-            return;
+            if (data == null)
+            {
+                Debug.LogError("Sheet is null");
+                return;
+            }
+            string rowStr = inputField.text;
+            if (String.IsNullOrEmpty(rowStr))
+            {
+                output.text = "Input: row number";
+                return;
+            }
+            Dictionary<string, IGridData> rowData = data.getRow(int.Parse(rowStr));
+            output.text = formatRow(rowData);
         }
-        int rowNo = int.Parse(searchParams[0]);
-        string columnName = searchParams[1];
-        string value = searchParams[2];
 
-        _data.modify(columnName, rowNo, new GridData<string>(value));
-        EditorUtility.SetDirty(_data);
+        private void onGetRow2BtnClick()
+        {
+            if (data == null)
+            {
+                Debug.LogError("Sheet is null");
+                return;
+            }
+            string rowStr = inputField.text;
+            if (String.IsNullOrEmpty(rowStr))
+            {
+                output.text = "Input: title|value (only string)";
+                return;
+            }
+            string[] searchParams = rowStr.Split('|');
+            if (searchParams.Length != 2)
+            {
+                Debug.LogError("searchParams error");
+                return;
+            }
+            List<Dictionary<string, IGridData>> rows = data.getRows(searchParams[0], searchParams[1]);
+            if (rows == null)
+            {
+                output.text = "No result";
+                return;
+            }
+
+            string outputStr = "";
+            for (int i = 0; i < rows.Count; i++)
+            {
+                outputStr += formatRow(rows[i]);
+                outputStr += "-----------------------------------\n";
+            }
+
+            output.text = outputStr;
+        }
+
+        private void onModifyBtnClick()
+        {
+            string[] searchParams = inputField.text.Split('|');
+            if (searchParams.Length != 3)
+            {
+                output.text = "Input: rowNo|colName|value (only string)";
+                return;
+            }
+            int rowNo = int.Parse(searchParams[0]);
+            string columnName = searchParams[1];
+            string value = searchParams[2];
+
+            data.modify(columnName, rowNo, new GridData<string>(value));
+            EditorUtility.SetDirty(data);
+        }
     }
 }
+
+
